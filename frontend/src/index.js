f19b459e4dbd6541d333a130f91c7038776f585b
XLG123 <xiaolin.guan123@gmail.com>
2023-07-21 10:15:04 -0400

    created database and executed migrations to database

---
+++ frontend/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+
+function Root() {
+  return (
+    <Provider>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
