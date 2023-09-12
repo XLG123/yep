368e5fa245f51a5f869ace23e201ff9782764511
XLG123 <98980493+XLG123@users.noreply.github.com>
2023-09-12 15:43:41 -0400

    removed state in console for production mode

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -8,14 +8,18 @@ const rootReducer = combineReducers({
   restaurants,
 });
 
-const logger = require("redux-logger").default;
-const composeEnhancers =
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let enhancer;
 
-const enhancer = composeEnhancers(applyMiddleware(thunk, logger));
-
-const configureStore = (preloadedState = {}) => {
-  return createStore(rootReducer, preloadedState, enhancer);
+if (process.env.NODE_ENV === "production") {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = require("redux-logger").default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
-export default configureStore;
+export default function configureStore(preloadedState = {}) {
+  return createStore(rootReducer, preloadedState, enhancer);
+}
