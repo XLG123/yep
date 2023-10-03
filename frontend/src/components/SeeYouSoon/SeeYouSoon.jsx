ce90141e4376039c7c7bacbe4bd0185ad11e58f1
XLG123 <98980493+XLG123@users.noreply.github.com>
2023-10-02 22:40:41 -0400

    See You Soon Page is done and fixed minor css styling on search bar dropdown menu

--- frontend/src/components/SeeYouSoon/SeeYouSoon.jsx
+++ frontend/src/components/SeeYouSoon/SeeYouSoon.jsx
@@ -4,9 +4,12 @@ import toast, { Toaster } from "react-hot-toast";
 import IosShareIcon from "@mui/icons-material/IosShare";
 import PortraitIcon from "@mui/icons-material/Portrait";
 import DescriptionIcon from "@mui/icons-material/Description";
+import AddIcCallIcon from "@mui/icons-material/AddIcCall";
+import EmailIcon from "@mui/icons-material/Email";
 import ResumePDF from "../../assets/files/resume.pdf";
 import "./SeeYouSoon.css";
 import { NavLink } from "react-router-dom";
+import Tooltip from "@mui/material/Tooltip";
 
 const SeeYouSoon = () => {
   const handleCopyLink = (e) => {
@@ -26,64 +29,137 @@ const SeeYouSoon = () => {
   };
 
   return (
-    <div className="see-you-soon-container">
-      <div className="see-you-soon-content">
-        <div className="see-you-soon-text">
-          <div className="see-you-soon-title">
-            <div>Heading out?</div>
-            <div>Thanks for visiting Yep!</div>
-          </div>
-          <div className="see-you-soon-body">
-            <p>
-              Enjoy using Yep and want to learn more about the developer behind
-              this project? Feel free to share the link with others and get in
-              touch.
-            </p>
-            <div className="contact-btns">
-              <CopyToClipboard text="https://yep-ech2.onrender.com/">
-                <div>
-                  <div
-                    className="hp-share-btn"
-                    onClick={(e) => handleCopyLink(e)}
-                  >
-                    <IosShareIcon
-                      sx={{ fontSize: "1.5vw", marginRight: "0.3em" }}
-                      className="sp-btn-icon"
-                    />
-                    Share
+    <>
+      <div className="see-you-soon-line-break"></div>
+      <div className="see-you-soon-container">
+        <div className="see-you-soon-content">
+          <div className="see-you-soon-text">
+            <div className="see-you-soon-title">
+              <div>Heading out?</div>
+              <div>Thanks for visiting Yep!</div>
+            </div>
+            <div className="see-you-soon-body">
+              <p>
+                Enjoy using Yep and want to learn more about the developer
+                behind this project? Feel free to share the link with others and
+                get in touch.
+              </p>
+              <div className="contact-btns">
+                <CopyToClipboard text="https://yep-ech2.onrender.com/">
+                  <div>
+                    <div
+                      className="hp-share-btn"
+                      onClick={(e) => handleCopyLink(e)}
+                    >
+                      <IosShareIcon
+                        sx={{ fontSize: "1.5vw", marginRight: "0.3em" }}
+                        className="sp-btn-icon"
+                      />
+                      Share
+                    </div>
+                    <Toaster />
                   </div>
-                  <Toaster />
+                </CopyToClipboard>
+                <NavLink
+                  to="https://xlg123.github.io/"
+                  className="portfolio-btn"
+                >
+                  <PortraitIcon
+                    sx={{ fontSize: "1.5vw", marginRight: "0.3em" }}
+                  />
+                  Portfolio
+                </NavLink>
+                <a
+                  href={ResumePDF}
+                  download="Xiao-Lin-Guan-Resume"
+                  rel="noreferrer"
+                  className="resume-btn"
+                >
+                  <DescriptionIcon
+                    sx={{ fontSize: "1.5vw", marginRight: "0.3em" }}
+                  />
+                  Resume
+                </a>
+              </div>
+              <div className="developer-tel-email-container">
+                <div className="developer-tel-container">
+                  <div className="developer-tel">+1 (646)-750-5019</div>
+
+                  <Tooltip
+                    title="Make a call"
+                    placement="top"
+                    componentsProps={{
+                      tooltip: {
+                        sx: {
+                          backgroundColor: "#999",
+                          fontSize: "0.9vw",
+                          textAlign: "justify",
+                          padding: "0.5em",
+                          lineHeight: "1.1em",
+                        },
+                      },
+                      arrow: { sx: { color: "#999" } },
+                    }}
+                    arrow
+                  >
+                    <a href="tel:+6467505019" className="contact-link">
+                      <AddIcCallIcon
+                        sx={{
+                          fontSize: "1.4vw",
+                          color: "#999",
+                          "&:hover": { color: "#777" },
+                        }}
+                      />
+                    </a>
+                  </Tooltip>
                 </div>
-              </CopyToClipboard>
-              <NavLink to="https://xlg123.github.io/" className="portfolio-btn">
-                <PortraitIcon
-                  sx={{ fontSize: "1.5vw", marginRight: "0.3em" }}
-                />
-                Portfolio
-              </NavLink>
-              <a
-                href={ResumePDF}
-                download="Xiao-Lin-Guan-Resume"
-                rel="noreferrer"
-                className="resume-btn"
-              >
-                <DescriptionIcon
-                  sx={{ fontSize: "1.5vw", marginRight: "0.3em" }}
-                />
-                Resume
-              </a>
-            </div>
-            <div className="developer-tel-email">
-              <input placeholder="+1 (646)-750-5019"/>
+                <div className="developer-email-container">
+                  <div className="developer-email">
+                    xiaolin.guan123@gmail.com
+                  </div>
+                  
+                  <Tooltip
+                    title="Send an email"
+                    placement="bottom"
+                    componentsProps={{
+                      tooltip: {
+                        sx: {
+                          backgroundColor: "#999",
+                          fontSize: "0.9vw",
+                          textAlign: "justify",
+                          padding: "0.5em",
+                          lineHeight: "1.1em",
+                        },
+                      },
+                      arrow: { sx: { color: "#999" } }, 
+                    }}
+                    arrow
+                  >
+
+                    <a
+                      href="mailto: xiaolin.guan123@gmail.com"
+                      className="contact-link"
+                    >
+                      <EmailIcon
+                        sx={{
+                          fontSize: "1.4vw",
+                          color: "#999",
+                          "&:hover": { color: "#777" },
+                        }}
+                      />
+                    </a>
+                  </Tooltip>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="see-you-soon-picture">
-          <img src={SeeYouSoonImage} alt="see you soon frame" />
+          <div className="see-you-soon-picture">
+            <img src={SeeYouSoonImage} alt="see you soon frame" />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
